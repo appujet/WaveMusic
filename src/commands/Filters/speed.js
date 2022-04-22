17fc5b3ad0a115c9a4b2ecdb819359a7dacd179b
Abhijit <90694822+AkAbhijit@users.noreply.github.com>
2022-04-22 10:01:13 +0530

    Update speed.js

--- src/commands/Filters/speed.js
+++ src/commands/Filters/speed.js
@@ -15,26 +15,95 @@ module.exports = {
   sameVoiceChannel: true,
   execute: async (message, args, client, prefix) => {
 
-    const player = client.manager.players.get(message.guild.id);
-
-    if (!player.current) {
-      let thing = new MessageEmbed().setColor('RED').setDescription('There is no music playing.');
+    const player = message.client.manager.get(message.guild.id);
+    if (!player.queue.current) {
+      let thing = new MessageEmbed()
+        .setColor("RED")
+        .setDescription("There is no music playing.");
       return message.reply({ embeds: [thing] });
     }
-    const emojiequalizer = client.emoji.filter;
-    await player.player.setFilters({
-      op: 'filters',
-      guildId: message.guild.id,
-      timescale: {
-        speed: 1.501,
-        pitch: 1.245,
-        rate: 1.921,
-      },
-    });
-    let thing = new MessageEmbed()
+    const emojiequalizer = message.client.emoji.filter;
+    const embed = new MessageEmbed()
       .setColor(client.embedColor)
-      .setDescription(`${emojiequalizer} Speed Mode Is Enabled`);
+      .setDescription(`Chose The Buttons`);
 
-    return message.reply({ embeds: [thing] });
+    const but = new MessageButton()
+      .setCustomId("clear_but")
+      .setLabel("OFF")
+      .setStyle("DANGER");
+    const but2 = new MessageButton()
+      .setCustomId("speed_but")
+      .setLabel("ON")
+      .setStyle("PRIMARY");
+
+    const but_ = new MessageButton()
+      .setCustomId("clear_but_")
+      .setLabel("OFF")
+      .setStyle("DANGER")
+      .setDisabled(true);
+    const but_2 = new MessageButton()
+      .setCustomId("speed_but_")
+      .setLabel("ON")
+      .setStyle("PRIMARY")
+      .setDisabled(true);
+
+    const row1 = new MessageActionRow().addComponents(but, but_2);
+    const row2 = new MessageActionRow().addComponents(but2, but_);
+    const row3 = new MessageActionRow().addComponents(but2, but_);
+    const m = await message.reply({ embeds: [embed], components: [row3] });
+
+    const embed1 = new MessageEmbed().setColor(client.embedColor);
+    const collector = m.createMessageComponentCollector({
+      filter: (f) =>
+        f.user.id === message.author.id
+          ? true
+          : false && f.deferUpdate().catch(() => {}),
+      time: 60000,
+      idle: 60000 / 2,
+    });
+    collector.on("end", async () => {
+      if (!m) return;
+      await m
+        .edit({
+          embeds: [
+            embed1.setDescription(`Time is Out type again ${prefix}speed`),
+          ],
+          components: [
+            new MessageActionRow().addComponents(
+              but2.setDisabled(true),
+              but.setDisabled(true)
+            ),
+          ],
+        })
+        .catch(() => {});
+    });
+    collector.on("collect", async (b) => {
+      if (!b.replied) await b.deferUpdate({ ephemeral: true });
+      if (b.customId === "clear_but") {
+        await player.player.clearFilters();
+        return await b.editReply({
+          embeds: [
+            embed1.setDescription(`${emojiequalizer} Speed Mode is \`OFF\``),
+          ],
+          components: [row2],
+        });
+      } else if (b.customId === "speed_but") {
+        await player.player.setFilters({
+          op: 'filters',
+          guildId: message.guild.id,
+          timescale: {
+            speed: 1.501,
+            pitch: 1.245,
+            rate: 1.921,
+          },
+        });
+        return await b.editReply({
+          embeds: [
+            embed1.setDescription(`${emojiequalizer} Speed Mode is \`ON\``),
+          ],
+          components: [row1],
+        });
+      }
+    });
   },
 };
