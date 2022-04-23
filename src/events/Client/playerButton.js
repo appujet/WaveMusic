ed9f5551014ede52679ad462b0bb373f9584628d
AkAbhijit <abhijitkirtunia011@gmail.com>
2022-04-23 12:10:07 +0530

    Added Button in player

---
+++ src/events/Client/playerButton.js
@@ -0,0 +1,125 @@
+const { MessageEmbed } = require('discord.js');
+module.exports = {
+  name: 'interactionCreate',
+  run: async (client, interaction) => {
+    const player = client.manager.players.get(interaction.guild.id);
+    const emojipause = client.emoji.pause;
+    const emojiresume = client.emoji.resume;
+    const emojiskip = client.emoji.skip;
+    const volumeEmoji = client.emoji.volumehigh;
+    const previousEmoji = client.emoji.previous;
+
+    if (interaction.isButton()) {
+      await interaction.deferReply({
+        ephemeral: false,
+      });
+      if (interaction.customId === `${player.guild}pause`) {
+        if (player.player.paused) {
+          await player.setPaused(false);
+
+          await interaction.editReply({
+            embeds: [
+              new MessageEmbed()
+                .setDescription(
+                  `${emojiresume} [${player.current.title}](${player.current.uri}) is now unpaused/resumed.`,
+                )
+                .setColor(client.embedColor),
+            ],
+          }).then(msg => { setTimeout(() => { msg.delete() }, 3000) });
+        } else {
+          await player.setPaused(true);
+
+          await interaction.editReply({
+            embeds: [
+              new MessageEmbed()
+                .setDescription(
+                  `${emojipause} [${player.current.title}](${player.current.uri}) is now paused.`,
+                )
+                .setColor(client.embedColor),
+            ],
+          }).then(msg => { setTimeout(() => { msg.delete() }, 3000) });
+        }
+      } else if (interaction.customId === `${player.guild}skip`) {
+        if (player.queue.length === 0) {
+          let noskip = new MessageEmbed()
+            .setColor(client.embedColor)
+            .setDescription(`No more songs left in the queue to skip.`);
+        return interaction.editReply({ embeds: [noskip] }).then(msg => { setTimeout(() => { msg.delete() }, 3000) });
+        }
+         await player.player.stopTrack();
+          await interaction.editReply({
+          embeds: [
+            new MessageEmbed()
+              .setDescription(
+                `${emojiskip} Skipped - [${player.current.title}](${player.current.uri})`,
+              )
+              .setColor(client.embedColor),
+          ],
+        }).then(msg => { setTimeout(() => { msg.delete() }, 3000) });
+            
+        
+    }
+      else if (interaction.customId === `${player.guild}previous`) {
+        if (!player.previous) {
+          let noskip = new MessageEmbed()
+            .setColor(client.embedColor)
+            .setDescription(`No Previous song found`);
+          interaction.editReply({ embeds: [noskip] }).then(msg => { setTimeout(() => { msg.delete() }, 3000) });
+        }
+        if (player.previous) {
+          player.queue.unshift(player.previous);
+          await player.player.stopTrack();
+        }
+       await interaction.editReply({
+          embeds: [
+            new MessageEmbed()
+              .setDescription(
+                `${previousEmoji} Previous [${player.previous.title}](${player.previous.uri})`
+              )
+              .setColor(client.embedColor)
+          ],
+        }).then(msg => { setTimeout(() => { msg.delete() }, 3000) });
+      } else if (interaction.customId === `${player.guild}voldown`) {
+        let amount = Number(player.player.filters.volume * 100 - 10);
+        if (amount <= 10)
+          return await interaction.editReply({
+            embeds: [
+              new MessageEmbed()
+                .setColor(client.embedColor)
+                .setDescription(`Volume Cannot Decread \`[ 10% ]\``)
+            ],
+          }).then(msg => { setTimeout(() => { msg.delete() }, 3000) });
+        await player.setVolume(amount / 1);
+        interaction.editReply({
+          embeds: [
+            new MessageEmbed()
+              .setColor(client.embedColor)
+              .setDescription(
+                `${volumeEmoji} Volume set to: \`[ ${player.player.filters.volume * 100}% ]\``,
+              ),
+          ],
+        }).then(msg => { setTimeout(() => { msg.delete() }, 3000) });
+      } else if (interaction.customId === `${player.guild}volup`) {
+        let amount = Number(player.player.filters.volume * 100 + 10);
+        if (amount >= 100)
+          return await interaction.editReply({
+            embeds: [
+              new MessageEmbed()
+                .setColor(client.embedColor)
+                .setDescription(`Volume Cannot Exceed \`[ 100% ]\``),
+            ],
+          }).then(msg => { setTimeout(() => { msg.delete() }, 3000) });
+        await player.setVolume(amount / 1);
+        interaction.editReply({
+          embeds: [
+            new MessageEmbed()
+              .setColor(client.embedColor)
+              .setDescription(
+                `${volumeEmoji} Volume set to: \`[ ${player.player.filters.volume * 100}% ]\``,
+              ),
+          ],
+        }).then(msg => { setTimeout(() => { msg.delete() }, 3000) });
+      }
+    }
+}
+  };
--- src/events/Players/playerStart.js
+++ src/events/Players/playerStart.js
@@ -1,4 +1,4 @@
-const { MessageEmbed, Client } = require("discord.js");
+const { MessageEmbed, Client, MessageButton, MessageActionRow } = require("discord.js");
 const { convertTime } = require('../../utils/convert.js');
 
 module.exports = {
@@ -13,13 +13,21 @@ module.exports = {
 
 		const emojiplay = client.emoji.play;
 
+		const but1 = new MessageButton().setCustomId(`${player.guild}pause`).setEmoji(`⏸️`).setStyle('PRIMARY')
+		const but2 = new MessageButton().setCustomId(`${player.guild}previous`).setEmoji(`⏮️`).setStyle('PRIMARY')
+		const but3 = new MessageButton().setCustomId(`${player.guild}skip`).setEmoji(`⏭️`).setStyle('PRIMARY')
+		const but4 = new MessageButton().setCustomId(`${player.guild}voldown`).setEmoji(`🔉`).setStyle('PRIMARY')
+		const but5 = new MessageButton().setCustomId(`${player.guild}volup`).setEmoji(`🔊`).setStyle('PRIMARY')
+
+		const row = new MessageActionRow().addComponents(but4, but2, but1, but3, but5 )
+
 		const main = new MessageEmbed()
 			.setAuthor({ name: track.requester.tag, iconURL: track.requester.displayAvatarURL() })
 			.setDescription(`${emojiplay} Now Playing - [${track.title}](${track.uri}) - \`[ ${track.isStream ? '[**◉ LIVE**]' : convertTime(player.current.length)} ]\``)
 			.setColor(client.embedColor)
 			.setTimestamp()
 			.setThumbnail(`${track.thumbnail ? track.thumbnail : `https://img.youtube.com/vi/${player.current.identifier}/hqdefault.jpg`}`)
-           client.channels.cache.get(player.text).send({ embeds: [main] }).then(x => player.data.set("message", x));
-	
+           client.channels.cache.get(player.text).send({ embeds: [main] , components: [row] }).then(x => player.data.set("message", x));
+		   const m =  player.data.get("message")
 	}
 };
